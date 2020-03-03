45cef177ba34459e956270ee085edbb4885a4d62
Kai Sells <kai.sells@hotmail.co.uk>
2020-03-03 18:26:44 +0000

    Updates

---
+++ precache-manifest.a8e8d38ab94845673831ae81bbb0dcc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f044a6aad75834623f761eb0db67ba4d",
+    "url": "/bestselling-books-react/index.html"
+  },
+  {
+    "revision": "8cb474482cb672b732a7",
+    "url": "/bestselling-books-react/static/css/main.483f295f.chunk.css"
+  },
+  {
+    "revision": "9a65a19877a8134eb6c3",
+    "url": "/bestselling-books-react/static/js/2.845b1f8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bestselling-books-react/static/js/2.845b1f8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb474482cb672b732a7",
+    "url": "/bestselling-books-react/static/js/main.627982c7.chunk.js"
+  },
+  {
+    "revision": "b88b65ceafc503379d92",
+    "url": "/bestselling-books-react/static/js/runtime-main.bd1af113.js"
+  }
+]);
